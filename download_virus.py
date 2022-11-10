3cc47b9d2c4f853df56c8eb2f6b5324fd0f46f27
winstonsmith1897 <107133112+winstonsmith1897@users.noreply.github.com>
2022-11-10 15:27:00 +0100

    Update download_virus.py

--- download_virus.py
+++ download_virus.py
@@ -4,7 +4,7 @@ from termcolor import colored
 import subprocess
 
 burp0_url = "https://virusshare.com:443/search"
-burp0_cookies = {"SESSID": "phfn57mcio9slg04h8l1v9u5o5"}
+burp0_cookies = {"SESSID": "session_cookie"}
 burp0_headers = {"Cache-Control": "max-age=0", "Sec-Ch-Ua": "\"Not;A=Brand\";v=\"99\", \"Chromium\";v=\"106\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"", "Upgrade-Insecure-Requests": "1", "Origin": "https://virusshare.com", "Content-Type": "application/x-www-form-urlencoded", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.62 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Referer": "https://virusshare.com/", "Accept-Encoding": "gzip, deflate", "Accept-Language": "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7", "Connection": "close"}
 
 print('Reading hashes... This could take few minutes.')
